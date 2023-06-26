19a1d0e138fa997ad7a160fb00d9582f72aa6b5e
Yevhenii Kalinichenko <105106072+djek195@users.noreply.github.com>
2023-06-26 19:12:52 +0300

    #30 Created CatalogPage
    1. Been modified Card.js and Card.modules.css
    2. Created Catalog page component
    3. Prepared place for other future components.
    4. Add test elements, but they will be replace by working components.

---
+++ src/components/Card/Card.js
@@ -0,0 +1,31 @@
+import styles from "./Card.module.css";
+import { ReactComponent as BasketIcon } from "./basket-icon.svg";
+const Card = ({ product }) => {
+  const { title, image, alt, price } = product;
+
+  console.log("product");
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardWrapper}>
+        <div className={styles.imgWrapper}>
+          <img src={image} alt={alt} />
+        </div>
+      </div>
+
+      <div className={styles.cardTitle}>
+        <h3>{title}</h3>
+      </div>
+      <div className={styles.cardInfo}>
+        <div className={styles.cardPriceGroup}>
+          <p className={styles.cardPriceUah}>{`${price} ₴`}</p>
+        </div>
+        <div className={styles.cardBasket}>
+          <BasketIcon className={styles.cardBasketIcon} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
