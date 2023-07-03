56daff76872d1e23f1da1ff242551e66f412cd07
galyna-k <hkzlvs@gmail.com>
2023-07-03 15:20:24 +0100

    added verification of info about the current user

--- src/components/AuthProvider/AuthProvider.js
+++ src/components/AuthProvider/AuthProvider.js
@@ -1,15 +1,15 @@
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const AUTH0_USER = process.env.REACT_APP_AUTH0_DOMAIN
+const AUTH0_USER_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN
 const AUTH0_KEY = process.env.REACT_APP_AUTH0_CLIENT_ID
 
 const providerConfig = {
-  domain: AUTH0_USER,
+  domain: AUTH0_USER_DOMAIN,
   clientId: AUTH0_KEY,
   authorizationParams: {
-     redirect_uri: window.location.origin
-  }
- }
+    redirect_uri: window.location.origin,
+  },
+};
 
 const AuthProvider = ({children}) => {
   return (
--- src/components/Authentication/Authentication.js
+++ src/components/Authentication/Authentication.js
@@ -1,10 +1,56 @@
+import { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 import HeaderButton from '../HeaderButton/HeaderButton';
 
-const Authentication = ({ style }) => {
-  const { error, isLoading, isAuthenticated, user, loginWithRedirect, logout } = useAuth0();
+const AUTH0_USER_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN;
 
+const Authentication = ({ style }) => {
+  const {
+    error,
+    isLoading,
+    isAuthenticated,
+    user,
+    getAccessTokenSilently,
+    loginWithRedirect,
+    logout,
+  } = useAuth0();
+
+  const [userMetaData, setUserMetaData] = useState(null);
+
+  useEffect(() => {
+      const getUserMetaData = async () => {
+        const domain = AUTH0_USER_DOMAIN;
+        try {
+          const accessToken = await getAccessTokenSilently({
+            authorizationParams: {
+               audience: `https://${domain}/api/v2/`,
+               scope: 'read:current_user',
+            },
+           });
+
+          const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+
+          const dataResponse = await fetch(userDetailsByIdUrl, {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              },
+            });
+
+          const { user_metadata } = await dataResponse.json();
+
+          setUserMetaData(user_metadata);
+        } catch (error) {
+          console.log(error.message);
+        }
+      };
+      getUserMetaData();
+    }, [getAccessTokenSilently, user?.sub]);
+
+  if (userMetaData) {
+    return <div>Loading data...</div>;
+  }
+  
   if (error) {
     return <div>Oops... {error.message}</div>;
   }
@@ -18,7 +64,7 @@ const Authentication = ({ style }) => {
       <HeaderButton
         onClick={() => {
           !isAuthenticated
-            ? loginWithRedirect()
+            ? loginWithRedirect({ returnTo: "/profile" })
             : logout({ returnTo: window.location.origin });
         }}
         icon="line-md:account"
