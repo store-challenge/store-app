9fcf9fdd2e58530e98ca0b4508671645c5657382
galyna-k <hkzlvs@gmail.com>
2023-07-03 14:05:53 +0100

    moved all data arrays to a separate folder
    this data will be obtained from the backend in the future

---
+++ src/data/index.js
@@ -0,0 +1,5 @@
+import categories from './categories.json';
+import subcategories from './subcategories.json';
+import products from './products.json'
+
+export {categories, subcategories, products}
