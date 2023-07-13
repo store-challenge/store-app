d67c1fdb329ddfd3e04ca8bc772e1dce9165e03f
Yevhenii Kalinichenko <105106072+djek195@users.noreply.github.com>
2023-07-13 23:03:42 +0300

    fixed footer component
    changed styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,22 @@
-import styles from "./Footer.module.css";
 import { Icon } from "@iconify/react";
+import styles from "./Footer.module.css";
+import logo from "../../logo.svg";
 
 const Footer = () => {
-  const phoneIcon = <Icon className={styles.icon} icon={"fluent:call-12-filled"} />;
+  const phoneIcon = (
+    <Icon className={styles.icon} icon={"fluent:call-12-filled"} />
+  );
   const mailIcon = <Icon className={styles.icon} icon={"uiw:mail"} />;
 
   const informationList = [
     { label: "Про нас", href: "/about", icon: null },
     { label: "Умови використання сайту", href: "/terms", icon: null },
     { label: "+380 939 475 397", href: "tel:+380939475397", icon: phoneIcon },
-    { label: "megamarket@market.com", href: "mailto:megamarket@market.com", icon: mailIcon },
+    {
+      label: "megamarket@market.com",
+      href: "mailto:megamarket@market.com",
+      icon: mailIcon,
+    },
   ];
 
   const deliveryList = [
@@ -21,7 +28,7 @@ const Footer = () => {
     <footer className={styles.wrapper}>
       <div className={styles.content}>
         <div className={styles.logoAndTime}>
-          <img src="" alt="MEGASTORE" />
+          <img src={logo} alt="MEGASTORE" />
           <p>Пн-Нд 09:00-20:00</p>
         </div>
         <div className={styles.info}>
