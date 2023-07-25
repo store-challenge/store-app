e60d6319132e2a06a0ea96bc90cbb0ae12c19df2
Yevhenii Kalinichenko <105106072+djek195@users.noreply.github.com>
2023-07-25 16:48:58 +0300

    Added Eouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LangProvider from './providers/LangProvider';
 import Header from './components/Header/Header';
 import CatalogPage from './pages/CatalogPage/CatalogPage';
+import CategoryPage from './pages/CategoryPage/CategoryPage';
 import Footer from './components/Footer/Footer';
 
 const theme = createTheme({
@@ -25,7 +26,12 @@ function App() {
       <LangProvider>
         <div className="App">
           <Header />
-          <CatalogPage />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<CatalogPage />} />
+              <Route path="/category" element={<CategoryPage />} />
+            </Routes>
+          </BrowserRouter>
           <Footer />
         </div>
       </LangProvider>
