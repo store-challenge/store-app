f9aa9afae4a69bf77306913afe4574dcaebe658a
galyna-k <79842465+halyna-k@users.noreply.github.com>
2023-07-28 23:01:34 +0300

    added temporary array of hot products

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,4 @@
 export { default as categories } from './categories.json';
 export { default as products } from './products.json';
+export { default as hotProducts } from './hotProducts.json';
 export { default as subcategories } from './subcategories.json';
