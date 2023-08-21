3eeaba67a9f9b7b4a198aa5e2060293d4920a423
Yevhenii <djek195@gmail.com>
2023-08-22 01:37:41 +0300

    fixed card component
    1. Changed size product image and hover
    2. Added limit of showing product name
    3. Added hint when the user hovered the mouse over the product name.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,18 +3,27 @@ import { Link } from 'react-router-dom';
 import styles from './card.module.css';
 
 const Card = ({ product }) => {
-  const { href, title, image, alt, price } = product;
+  const { href, title, image, price } = product;
+
+  let maxTitleLength = 15;
+  if (window.innerWidth >= 630 && window.innerWidth <= 1023) {
+    maxTitleLength = 12;
+  } else if (window.innerWidth >= 1023) {
+    maxTitleLength = 19;
+  }
+
+  const trimmedTitle = title.length > maxTitleLength ? `${title.slice(0, maxTitleLength)}...` : title;
 
   return (
     <Link to={href}>
       <div className={styles.wrapper}>
         <div className={styles.card}>
           <div className={styles.productImage}>
-            <img src={image} alt={alt} />
+            <img src={image} alt={'product image'} />
           </div>
         </div>
         <div className={styles.title}>
-          <h3>{title}</h3>
+          <h3 title={title}>{trimmedTitle}</h3>
         </div>
         <div className={styles.info}>
           <div className={styles.priceGroup}>
