b5614464920f4a4a3d625cf964bbf59f703e36e3
galyna-k <79842465+halyna-k@users.noreply.github.com>
2023-09-17 21:10:45 +0100

    created Quantity component with styles

---
+++ src/components/Quantity/Quantity.styled.js
@@ -0,0 +1,29 @@
+import { styled } from '@mui/material/styles';
+import { ButtonGroup, Typography } from '@mui/material';
+
+export const CustomButtonGroup = styled(ButtonGroup)(({ theme }) => ({
+  minWidth: '176px',
+  width: '-webkit-fill-available',
+  height: '40px',
+  justifyContent: 'center',
+  alignItems: 'center',
+  border: '1px solid var(--mainColor)',
+  borderRadius: '15px',
+  color: 'var(--mainColor)',
+  backgroundColor: 'var(--secondColor)',
+  [theme.breakpoints.down('xl')]: {
+    width: '286px',
+    fontSize: '15px',
+    fontWeight: '500',
+  },
+}));
+
+export const CustomTypography = styled(Typography)(() => ({
+  fontFamily: 'Montserrat',
+  fontSize: '20px',
+  fontStyle: 'normal',
+  fontWeight: '400',
+  lineHeight: '95%',
+  letterSpacing: '0.6px',
+  color: 'var(--mainColor)',
+}));
