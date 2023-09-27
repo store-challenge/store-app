87f04389ed9383fae9f30b06e43f1aee4e1cd939
galyna-k <79842465+halyna-k@users.noreply.github.com>
2023-09-27 11:29:09 +0100

    fixed Price section

--- src/components/PriceSection/PriceSection.js
+++ src/components/PriceSection/PriceSection.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2/Grid2';
 
 import Quantity from '../Quantity/Quantity';
@@ -23,45 +23,33 @@ const PriceSection = props => {
   };
 
   return (
-    <Grid
-      container
-      rowGap={breakpoint ? 6.25 : 2.5}
-      columnGap={20}
-      marginTop={breakpoint ? 6.25 : 2.5}
-      marginLeft={breakpoint && 10}
-      width={breakpoint ? '176px' : '288px'}>
-      <Grid>
-        <Typography
-          variant="paragraph"
-          style={styles}
-          color={available ? 'var(--mainColor)' : '#6b4c7d40'}
-          sx={{ fontWeight: breakpoint && '300' }}>
-          {available ? `В наявності ${available}` : 'Не в наявності'}
-        </Typography>
-      </Grid>
-      <Grid>
-        <Typography variant="paragraph" style={styles} color={'var(--priceTextColor)'}>
-          {price && `${price.toLocaleString()} ₴`}
-        </Typography>
-      </Grid>
-      <Grid>
+    <Grid container flexDirection={'column'} rowGap={breakpoint ? 6.25 : 2.5} maxWidth={'100%'}>
+      <Typography
+        variant="paragraph"
+        style={styles}
+        color={available ? 'var(--mainColor)' : '#6b4c7d40'}
+        sx={{ fontWeight: breakpoint && '300' }}>
+        {available ? `В наявності ${available}` : 'Не в наявності'}
+      </Typography>
+      <Typography variant="paragraph" style={styles} color={'var(--priceTextColor)'}>
+        {price && `${price.toLocaleString()} ₴`}
+      </Typography>
+      <Box display={'flex'} flexDirection={'column'} alignItems={'center'} rowGap={'20px'}>
         <Quantity style={styles} currentQuantity={available} />
-      </Grid>
-      <Grid>
         <ButtonCustom
           disabled={cartQuantity >= available}
           onClick={handleBuyClick}
           text={'Купити'}
           sx={{
+            margin: 0,
             backgroundColor: 'var(--mainColor)',
             color: 'var(--secondColor)',
-            margin: 0,
             '&.Mui-disabled': {
               backgroundColor: '#6b4c7d40',
             },
           }}
         />
-      </Grid>
+      </Box>
     </Grid>
   );
 };
