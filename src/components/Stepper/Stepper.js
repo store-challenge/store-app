89f8ec6c5e1d26b2c18b8d57b6929844d10414ef
galyna-k <79842465+halyna-k@users.noreply.github.com>
2023-09-27 11:41:34 +0100

    added Images gallery with Stepper

---
+++ src/components/Stepper/Stepper.js
@@ -0,0 +1,33 @@
+import { Icon } from '@iconify/react';
+
+import { CustomMobileStepper, CustomIconButton } from './Stepper.styled';
+
+const Stepper = props => {
+  const { array, currentIndex, setCurrentIndex } = props;
+
+  const handlePrevious = () => setCurrentIndex(index => (index + array.length - 1) % array.length);
+
+  const handleNext = () => setCurrentIndex(index => (index + 1) % array.length);
+
+  return (
+    <CustomMobileStepper
+      variant="dots"
+      steps={array.length}
+      position="static"
+      activeStep={currentIndex}
+      sx={{ maxWidth: 400, flexGrow: 1 }}
+      nextButton={
+        <CustomIconButton onClick={handleNext} disabled={array.length === 0}>
+          <Icon icon="iconamoon:arrow-right-2-duotone" color="var(--secondColor)" fontSize="20px" />
+        </CustomIconButton>
+      }
+      backButton={
+        <CustomIconButton onClick={handlePrevious} disabled={array.length === 0}>
+          <Icon icon="iconamoon:arrow-left-2-duotone" color="var(--secondColor)" fontSize="20px" />
+        </CustomIconButton>
+      }
+    />
+  );
+};
+
+export default Stepper;
