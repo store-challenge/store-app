b45db49c8d054c36008b16f8e776c11471e83162
galyna-k <79842465+halyna-k@users.noreply.github.com>
2023-09-29 11:22:00 +0100

    fixed Quantity component

--- src/components/Quantity/Quantity.js
+++ src/components/Quantity/Quantity.js
@@ -1,20 +1,19 @@
-import React, { useState } from 'react';
 import { Icon } from '@iconify/react';
 import { IconButton } from '@mui/material';
 import { CustomButtonGroup, CustomTypography } from './Quantity.styled';
 
-const Quantity = ({ currentQuantity }) => {
-  const [quantity, setQuantity] = useState(1);
+const Quantity = props => {
+  const { currentQuantity, onChange, available } = props;
 
   const handleIncrement = () => {
-    if (quantity < currentQuantity) {
-      setQuantity(quantity + 1);
+    if (currentQuantity < available) {
+      onChange(currentQuantity + 1);
     }
   };
 
   const handleDecrement = () => {
-    if (quantity > 1) {
-      setQuantity(quantity - 1);
+    if (currentQuantity > 1) {
+      onChange(currentQuantity - 1);
     }
   };
 
@@ -23,7 +22,7 @@ const Quantity = ({ currentQuantity }) => {
       <IconButton aria-label="reduce" onClick={handleDecrement} sx={{ padding: 0 }}>
         <Icon icon={'bi:dash-circle-fill'} width={'18px'} height={'18px'} color="var(--mainColor)" />
       </IconButton>
-      <CustomTypography marginX={'30px'}>{quantity}</CustomTypography>
+      <CustomTypography marginX={'30px'}>{currentQuantity}</CustomTypography>
       <IconButton aria-label="increase" onClick={handleIncrement} sx={{ padding: 0 }}>
         <Icon icon={'bi:plus-circle-fill'} width={'18px'} height={'18px'} color="var(--mainColor)" />
       </IconButton>
