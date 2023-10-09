57957fc73de5a875d72640f0014f44073c6b8b2b
galyna-k <hkzlvs@gmail.com>
2023-10-09 21:27:15 +0100

    updated  the Title component by adding a SubTitle

--- src/components/Title/Title.styled.js
+++ src/components/Title/Title.styled.js
@@ -4,17 +4,31 @@ import { Typography } from '@mui/material';
 const styles = {
   fontFamily: 'Montserrat',
   fontStyle: 'normal',
-  fontWeight: '300',
+  fontWeight: 500,
   lineHeight: '130%',
 };
 
-export const CustomTypography = styled(Typography)(({ theme }) => ({
+export const CustomTypographyTitle = styled(Typography)(({ theme }) => ({
   ...styles,
   marginTop: '20px',
   marginBottom: '20px',
+  fontSize: '18px',
   fontWeight: 700,
   [theme.breakpoints.up('xl')]: {
     marginTop: '50px',
     marginBottom: '50px',
+    fontSize: '25px',
+  },
+}));
+
+export const CustomTypographySubTitle = styled(Typography)(({ theme }) => ({
+  ...styles,
+  marginTop: '20px',
+  marginBottom: '20px',
+  fontSize: '13px',
+  [theme.breakpoints.up('xl')]: {
+    marginTop: '50px',
+    marginBottom: '50px',
+    fontSize: '20px',
   },
 }));
