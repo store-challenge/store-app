d102509f2294ee0c3f0946dd51827b1f8c7f238b
galyna-k <hkzlvs@gmail.com>
2023-10-31 10:53:26 +0000

    fixed price and basket in the card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,31 +2,38 @@ import { Icon } from '@iconify/react';
 import { Link } from 'react-router-dom';
 import styles from './card.module.css';
 import { RoutesLinks } from '../../constant/constant';
+import { useCart } from '../../providers/CartProvider';
+import Price from '../PriceSection/Price';
 
-const Card = ({ product }) => {
-  const { id, title, image, price } = product;
+const Card = ({ product, breakpoint }) => {
+  const { id, title, image, price, discountPrice } = product;
+  const { addToCart } = useCart();
+
+  const handleBuyClick = () => {
+    addToCart(product, 1);
+  };
 
   return (
-    <Link to={`${RoutesLinks.PRODUCT_PAGE}/${id}`}>
-      <div className={styles.wrapper}>
-        <div className={styles.card}>
-          <div className={styles.productImage}>
-            <img src={image} alt={'product image'} width="100%" height="100%" className={styles.image} />
+    <>
+      <Link to={`${RoutesLinks.PRODUCT_PAGE}/${id}`}>
+        <div className={styles.wrapper}>
+          <div className={styles.card}>
+            <div className={styles.productImage}>
+              <img src={image} alt={'product image'} width="100%" height="100%" className={styles.image} />
+            </div>
+          </div>
+          <div className={styles.title}>
+            <h3 title={title}>{title}</h3>
           </div>
         </div>
-        <div className={styles.title}>
-          <h3 title={title}>{title}</h3>
-        </div>
-        <div className={styles.info}>
-          <div className={styles.priceGroup}>
-            <p className={styles.priceUah}>{`${price.toLocaleString()} ₴`}</p>
-          </div>
-          <div className={styles.basket}>
-            <Icon className={styles.basketIcon} icon={'ion:cart-outline'} />
-          </div>
+      </Link>
+      <div className={styles.info}>
+        <Price price={price} newPrice={discountPrice} breakpoint={breakpoint} />
+        <div className={styles.basket}>
+          <Icon type="button" onClick={handleBuyClick} className={styles.basketIcon} icon={'ion:cart-outline'} />
         </div>
       </div>
-    </Link>
+    </>
   );
 };
 
