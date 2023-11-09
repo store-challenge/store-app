f04cae721a660776ac93dbacd829a71a19552f28
galyna-k <hkzlvs@gmail.com>
2023-11-09 14:47:22 +0000

    updated the SearchList page
    updated according to new requests;
    added Loading;
    the selecting sorting result will remain current when the page is reloaded

--- src/pages/SearchListPage.js
+++ src/pages/SearchListPage.js
@@ -1,22 +1,30 @@
 import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
 import { Stack } from '@mui/material';
-import { useLocation } from 'react-router-dom';
+
+import Loading from '../components/Loading/Loading';
+import { Info } from '../components/Info/Info';
 import CatalogList from '../components/CatalogList/CatalogList';
-// import ButtonCustom from '../components/Button/ButtonCustom';
+import ButtonCustom from '../components/Button/ButtonCustom';
 import FilterDesktop from '../components/Filter/FilterDesktop';
 import FilterMobile from '../components/Filter/FilterMobile';
 import Sort from '../components/Sort/Sort';
 
-import { getSearchProducts } from '../services/getSearchProducts';
+import { getProductList } from '../services/getProducts';
+import { getBrandsList } from '../services/getBrands';
 
 const SearchListPage = ({ desktop }) => {
   const location = useLocation();
+  const navigate = useNavigate();
   const queryParams = new URLSearchParams(location.search);
-  const query = queryParams.get('query');
+  const productName = queryParams.get('query');
   const [searchList, setSearchList] = useState([]);
+  const [limit, setLimit] = useState(9);
   const [brands, setBrands] = useState([]);
   const [priceRange, setPriceRange] = useState([0, 20_000]);
   const [selectedBrand, setSelectedBrand] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const sortOptions = [
     { name: 'asc', sortBy: 'product_price', orderBy: 'ASC' },
@@ -25,30 +33,56 @@ const SearchListPage = ({ desktop }) => {
     { name: 'za', sortBy: 'product_title', orderBy: 'DESC' },
     { name: 'newest', sortBy: 'updated', orderBy: 'DESC' },
   ];
-  const [sort, setSort] = useState(sortOptions.length - 1);
+
+  const [sort, setSort] = useState(() => {
+    const savedSortName = queryParams.get('sort') || sortOptions.at(-1).name;
+    return sortOptions.find(option => option.name === savedSortName) || sortOptions.at(-1);
+  });
 
   useEffect(() => {
-    getSearchProducts(query)
-      .then(result => {
-        setSearchList(result);
+    const configProducts = { limit, productName, sortBy: sort.sortBy, orderBy: sort.orderBy };
+    const configBrands = { productName };
+
+    getProductList({ configProducts })
+      .then(searchResult => {
+        setSearchList(searchResult);
+        setLoading(false);
         return null;
       })
       .catch(error => {
         // eslint-disable-next-line no-console
         console.error('Виникла помилка при отриманні даних:', error);
+        setLoading(false);
         return null;
       });
-  }, [query]);
 
-  // const handleShowMore = () => {
-  //   setLimit(limit + 9);
-  // };
+    getBrandsList({ configBrands })
+      .then(brandsResult => {
+        setBrands(brandsResult);
+        return null;
+      })
+      .catch(error => {
+        // eslint-disable-next-line no-console
+        console.error('Виникла помилка при отриманні брендів:', error);
+        return null;
+      });
+
+    const updatedParams = new URLSearchParams(location.search);
+    updatedParams.set('sort', sort.name);
+    navigate(`?${updatedParams.toString()}`, { replace: true });
+  }, [limit, productName, sort.sortBy, sort.orderBy]);
+
+  const handleShowMore = () => {
+    setLimit(limit + 9);
+  };
 
   const handleSortChange = selectedOption => {
     sortOptions.map(element => element.name === selectedOption && setSort(element));
   };
 
-  return (
+  return loading ? (
+    <Loading />
+  ) : (
     <Stack>
       <Stack
         direction="row"
@@ -83,21 +117,35 @@ const SearchListPage = ({ desktop }) => {
             desktop={desktop}
           />
         )}
-        <Stack maxWidth={'100%'} direction="column" alignItems={desktop ? 'flex-end' : 'center'}>
-          <CatalogList products={searchList} />
-          {/* <ButtonCustom
-            sx={{
-              backgroundColor: desktop ? 'var(--mainColor)' : 'var(--secondColor)',
-              color: desktop ? 'var(--secondColor)' : 'var(--mainColor)',
-              marginTop: desktop ? '50px' : '20px',
-              '&.Mui-disabled': {
-                backgroundColor: '#6b4c7d40',
-              },
-            }}
-            disabled={limit < 9 || products.length % 9 !== 0}
-            onClick={handleShowMore}
-            text={'Показати ще'}
-          /> */}
+        <Stack width={'100%'} direction="column">
+          {searchList.length > 0 ? (
+            <Stack width={'100%'} alignItems={desktop ? 'flex-end' : 'center'}>
+              <CatalogList products={searchList} breakpoint={desktop} />
+              <ButtonCustom
+                sx={{
+                  backgroundColor: desktop ? 'var(--mainColor)' : 'var(--secondColor)',
+                  color: desktop ? 'var(--secondColor)' : 'var(--mainColor)',
+                  marginTop: desktop ? '50px' : '20px',
+                  '&.Mui-disabled': {
+                    backgroundColor: '#6b4c7d40',
+                  },
+                }}
+                disabled={limit < 9 || searchList.length % 9 !== 0}
+                onClick={handleShowMore}
+                text={'Показати ще'}
+              />
+            </Stack>
+          ) : (
+            <Stack width={'100%'} alignItems={'center'}>
+              <Info
+                variant={'p'}
+                text={`
+                За запитом
+                 "${productName}"
+                  нічого не знайдено.`}
+              />
+            </Stack>
+          )}
         </Stack>
       </Stack>
     </Stack>
