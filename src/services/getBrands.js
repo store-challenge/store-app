3e9e5fc6e13fc55a74a8a2f9d54f69115bca70e6
galyna-k <hkzlvs@gmail.com>
2023-11-18 16:53:17 +0000

    updated Endpoint to the Brands

--- src/services/getBrands.js
+++ src/services/getBrands.js
@@ -1,6 +1,6 @@
 import axiosClient from './axiosClient';
 
-const getBrandsEndpoint = 'api/v1/brand/byProducts';
+const getBrandsEndpoint = 'api/v1/brand/list';
 
 export const getBrandsList = async ({ configBrands }) => {
   const { data } = await axiosClient.get(getBrandsEndpoint, {
