b1298dc2175d1e292dfa6f1b745d8a059233ca3b
galyna-k <hkzlvs@gmail.com>
2023-11-27 21:36:01 +0000

    added icons to the payment types

--- src/data/images/index.js
+++ src/data/images/index.js
@@ -3,3 +3,8 @@ import adv2 from './adv2.svg';
 import adv3 from './adv3.svg';
 
 export const advertising = [adv1, adv2, adv3];
+export { default as NovaPostIcon } from './NovaPostIcon.svg';
+export { default as VisaIcon } from './visaIcon.svg';
+export { default as MastercardIcon } from './mastercardIcon.svg';
+export { default as GooglePayIcon } from './googlepayIcon.svg';
+export { default as ApplePayIcon } from './applepayIcon.svg';
