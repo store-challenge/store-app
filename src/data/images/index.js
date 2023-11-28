431237b051ddb920697506f3cf952380d826d455
galyna-k <hkzlvs@gmail.com>
2023-11-28 10:54:27 +0000

    fixed marginY in the Title, fixed images index

--- src/data/images/index.js
+++ src/data/images/index.js
@@ -3,7 +3,6 @@ import adv2 from './adv2.svg';
 import adv3 from './adv3.svg';
 
 export const advertising = [adv1, adv2, adv3];
-export { default as NovaPostIcon } from './NovaPostIcon.svg';
 export { default as VisaIcon } from './visaIcon.svg';
 export { default as MastercardIcon } from './mastercardIcon.svg';
 export { default as GooglePayIcon } from './googlepayIcon.svg';
